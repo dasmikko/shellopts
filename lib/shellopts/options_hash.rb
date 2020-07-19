37c8d6dcca960e4f4ed5afb81ec5c67fc5e0d917
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2020-07-19 06:25:54 +0200

    wip

--- lib/shellopts/options_hash.rb
+++ lib/shellopts/options_hash.rb
@@ -3,32 +3,29 @@ require "shellopts"
 module ShellOpts
   class OptionsHash
     def initialize(ast)
-      @hash = {}
-      @node_hash = {}
+      @hash = {} # Hash from Ast#name to [ast_node, option_value]
       for option in ast.options
         grammar = option.grammar
         value = grammar.argument? ? option.value : true
         if !@hash.key?(option.name) # Only true first this option is processed
           if grammar.repeated?
             value = [value]
-            node = [option]
           end
+          hash_value = [option, value]
           for key in grammar.short_names + grammar.long_names
-            @hash[key] = value
-            @node_hash[key] = node
+            @hash[key] = hash_value
           end
         else # Only happens if repeated?
-          @hash[key] << value
-          @node_hash[key] << node
+          @hash[key].last << value
         end
       end
       if ast = ast.command
-        @hash[ast.name] = @node_hash[ast.name] = OptionsHash.new(ast)
+        @hash[ast.name] = [ast, OptionsHash.new(ast)]
       end
     end
 
     def key?(key) @hash.key?(key) end
-    def [](key) @hash[key] end
+    def [](key) @hash[key]&.last end
     def keys() @hash.keys end
     def values() end
 
@@ -41,47 +38,3 @@ module ShellOpts
   end
 end
 
-__END__
-module ShellOpts
-  class OptionsHash
-    def initialize(ast)
-      @hash = {}
-      assign_hash(@hash, ast)
-    end
-
-
-  # a         true
-  # +a        [true, true, ...]
-  # a=        arg
-  # +a=       [arg1, arg2, ...]
-  # a=?       arg or nil
-  # +a=?      [arg1, nil, ...]
-            
-    def user(key) end
-    def count(key) Array(self[key]).size end
-    def key?(key) @hash.key?(key) end
-    def [](key) @hash[key]&.last end
-    def default(key, default_value) key?(key) ? self[key] : default_value end
-
-  private
-    def assign_hash(hash, ast)
-      for option in ast.options
-        grammar = option.grammar
-        value = grammar.argument? ? option.value : true
-        if !hash.key?(option.name) # Only true first this option is processed
-          value = [value] if grammar.repeated?
-          for key in grammar.short_names + grammar.long_names
-            hash[key] = [option.name, value]
-          end
-        else # Only happens if repeated?
-          hash[key].last << value
-        end
-      end
-      if ast = ast.command
-        hash[ast.name] = OptionsHash.new(ast)
-        hash = hash[ast.name].last
-      end
-      hash
-    end
-  end
-end
