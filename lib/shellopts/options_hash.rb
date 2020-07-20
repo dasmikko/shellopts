84f3c740fce029d2c710f7d9bc0e8eb2c25cf887
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2020-07-20 03:36:24 +0200

    eol

--- lib/shellopts/options_hash.rb
+++ lib/shellopts/options_hash.rb
@@ -2,39 +2,74 @@ require "shellopts"
 
 module ShellOpts
   class OptionsHash
+    attr_reader :ast
+
+    # @hash
+    #   grammar.keys => [ast, value]
+    #                => [[ast, value], ...]
+    #                => [ast, OptionsHash]
+
     def initialize(ast)
-      @hash = {} # Hash from Ast#name to [ast_node, option_value]
+      @ast = ast
+      @hash = {} # Hash from Ast keys to [ast_node, option_value(s)] TODO: Should be [grammar_node, ...]
       for option in ast.options
-        grammar = option.grammar
-        value = grammar.argument? ? option.value : true
+        value = [option, option.grammar.argument? ? option.value : true]
         if !@hash.key?(option.name) # Only true first this option is processed
-          if grammar.repeated?
-            value = [value]
-          end
-          hash_value = [option, value]
-          for key in grammar.short_names + grammar.long_names
-            @hash[key] = hash_value
-          end
+          value = [value] if option.grammar.repeated?
+#         hash_value = [option, value] # Ensure only one instance for multiple keys
+          option.grammar.names.each { |name| @hash[name] = value }
         else # Only happens if repeated?
-          @hash[key].last << value
+          @hash[option.name] << value
         end
       end
-      if ast = ast.command
-        @hash[ast.name] = [ast, OptionsHash.new(ast)]
+      if command = ast.command
+        @hash[command.name] = [command, OptionsHash.new(command)]
       end
     end
 
     def key?(key) @hash.key?(key) end
-    def [](key) @hash[key]&.last end
-    def keys() @hash.keys end
-    def values() end
+
+    def [](key) 
+      entry = @hash[key] or return nil
+      entry.first.is_a?(Array) ? entry.map(&:last) : entry.last
+    end
+
+    def size() keys.size end
+    def keys() @ast.options.map(&:name) + [@ast.command&.name].compact end
+    def values() 
+      p keys
+      keys.map { |key| self[key] } end
+
+    def name(key, index = nil)
+      p @hash.keys
+      p keys
+      p "------------------------------------------------------------"
+      if index.nil?
+        @hash[key].first.name
+      else
+        p @hash[key]
+        p @hash[key].last[index]
+        @hash[key].last[index].name
+      end
+    end
+
+#   def name(key, index = 0) node(key, index)&.name end
+#   def node(key, index = 0) @hash[key]&.first end
 
 
-    def default(key, default_value) key?(key) ? self[key] : default_value end
+#   def default(key, default_value) key?(key) ? self[key] : default_value end
 
-    def node(key, index = 0) end
-    def user(key) end
-    def count(key) Array(self[key]).size end
+  private
+    def value_of(key)
+      entry = @hash[key] or return nil
+      case entry.first
+        when Ast::Node; entry.last
+        when Array; entry.last
+      else
+        raise "Oops"
+      end
+    end
+
   end
 end
 
--- spec/options_hash_spec.rb
+++ spec/options_hash_spec.rb
@@ -13,6 +13,10 @@ describe ShellOpts::OptionsHash do
     expect(hash).to be_a ::ShellOpts::OptionsHash
   end
 
+  describe "#ast" do
+    it "returns the associated Ast object"
+  end
+
   describe "#key" do
     it "returns true iff the option is present" do
       hash = process2("a b", %w(-a))
@@ -67,7 +71,7 @@ describe ShellOpts::OptionsHash do
     end
     it "returns a ShellOpts::OptionHash value for commands" do
       hash = process2("A! B!", %w(A))
-      expect(hash["A"]).to be_a ShellOpts::OptionsHash
+      expect(hash["A"]).to be_a ::ShellOpts::OptionsHash
     end
     it "nests command options under the command hash" do
       hash = process2("A! a", %w(A -a))
@@ -75,12 +79,56 @@ describe ShellOpts::OptionsHash do
     end
   end
 
+  describe "#size" do
+    it "returns the number of options and commands" do
+      hash = process2("a,all b", %w(-a -b))
+      expect(hash.size).to eq 2
+    end
+  end
+
+  describe "#keys" do
+    it "returns an array of the used keys" do
+      hash = process2("a,all b", %w(-a -b))
+      expect(hash.keys).to eq %w(-a -b)
+    end
+  end
+
+  describe "#values" do
+    it "returns an array of values" do
+      hash = process2("+a= b= c=?", %w(-a ARG1 -a ARG2 -b ARG3 -c))
+      expect(hash.values).to eq [["ARG1", "ARG2"], "ARG3", nil]
+    end
+  end
+
+  describe "#name" do
+    it "returns the used option alias" do
+      hash = process2("a,all b,bee +c,C", %w(-a --bee -c -C))
+#     expect(hash.name("-a")).to eq "-a"
+#     expect(hash.name("--all")).to eq "-a"
+#     expect(hash.name("-b")).to eq "--bee"
+#     expect(hash.name("--bee")).to eq "--bee"
+
+#     p hash.keys
+#     p hash.values
+      puts "BEFORE"
+      expect(hash.name("-c", 1)).to eq "-C"
+
+    end
+  end
+
+  describe "#node" do
+    it "returns the Ast::Node object" do
+#     hash = process2("a,all +b,bee=", %w(-a -b B --bee=BEE))
+#     ast = hash.ast
+#     expect(hash.node("-a").grammar).to be ast.grammar.options["-a"]
+#     expect(hash.node("-b", 0).
 
 
-# it "returns an array of arguments for repeated options with arguments"
-# it "returns nil
-
-
-  
+#     expect(hash.name("-a")).to eq "-a"
+#     expect(hash.name("--all")).to eq "-a"
+#     expect(hash.name("-b")).to eq "--bee"
+#     expect(hash.name("--bee")).to eq "--bee"
+    end
+  end
 end
 
