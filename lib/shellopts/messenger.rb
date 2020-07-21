fc4fed595faef6a2f8574c26576ec456f6eeb423
Claus Rasmussen <claus.l.rasmussen@gmail.com>
2020-07-21 05:45:40 +0200

    Messenger ok

--- lib/shellopts/messenger.rb
+++ lib/shellopts/messenger.rb
@@ -1,33 +1,69 @@
 
 module ShellOpts
+  # Service object for output of messages
+  #
+  # Messages are using the common command line formats
+  #
   class Messenger
-    # Name of the program
-    attr_accessor :program
+    # Name of the program. When assigning to +name+ prefixed and suffixed
+    # whitespaces are removed
+    attr_accessor :name
 
-    # Usage string. If defined it is printed with a 'Usage: #@program ' prefix
+    # :nodoc:
+    def name=(name) @name = name.strip end
+    # :nodoc:
+
+    # Usage string. If not nil the usage string is printed by #error. When
+    # assigning to +usage+ suffixed whitespaces are removed and the format
+    # automatically set to +:custom+
     attr_accessor :usage
 
-    # Multiline description. If defined it will be printed instead of @usage
-    attr_accessor :description
+    # :nodoc:
+    def usage=(usage) 
+      @format = :custom
+      @usage = usage&.rstrip
+    end
+    # :nodoc:
 
-    def initialize(program, usage, description = nil)
-      @program = program
+    # Format of the usage string: +:default+ prefixes the +usage+ with 'Usage:
+    # #{name} ' before printing. +:custom+ prints +usage+ as is
+    attr_accessor :format
+
+    # Initialize a Messenger object. +name+ is the name of the name and +usage+
+    # is a short description of the options (eg. '-a -b') or a longer multiline
+    # explanation. The +:format+ option selects bewtween the two: +short+ (the
+    # default) or :long. Note that 
+    #
+    def initialize(name, usage, format: :default)
+      @name = name
       @usage = usage
-      @description = description
+      @format = format
     end
 
-    # Print error message and usage string and exit with status 1
+    # Print error message and usage string and exit with status 1. Output is
+    # using the following format
+    #
+    #   <name name>: <message>
+    #   Usage: <name name> <options and arguments>
+    #
     def error(*msgs)
-      $stderr.puts "#{program}: #{msgs.join}"
-      $stderr.puts description || usage if description || usage
+      $stderr.print "#{name}: #{msgs.join}\n"
+      if usage
+        $stderr.print "Usage: #{name} " if format == :default
+        $stderr.print "#{usage}\n"
+      end
       exit 1
     end
 
     # Print error message and exit with status 1. It use the current ShellOpts
     # object if defined. This method should not be called in response to
-    # user-errors but system errors (like disk full)
+    # user-errors but system errors (like disk full). Output is using the
+    # following format:
+    #
+    #   <name name>: <message>
+    #
     def fail(*msgs)
-      $stderr.puts "#{program}: #{msgs.join}"
+      $stderr.puts "#{name}: #{msgs.join}"
       exit 1
     end
   end
--- spec/messenger_spec.rb
+++ spec/messenger_spec.rb
@@ -1,56 +1,90 @@
 require 'spec_helper.rb'
 
-require 'shellopts/message'
+require 'shellopts/messenger'
 
 include ShellOpts
 
-describe ShellOpts::Message do
-  subject { Message.new("cmd", "-a -b") }
+describe ShellOpts::Messenger do
+  let(:message) { Messenger.new("cmd", "-a -b") }
+
+  describe "#name=" do
+    it "strips prefixed and suffixed whitespaces" do
+      message.name = " cmd "
+      expect(message.name).to eq "cmd"
+    end
+  end
+
+  describe "#usage=" do
+    it "strips suffixed whitespaces" do
+      message.usage = " usage "
+      expect(message.usage).to eq " usage"
+    end
+    it "sets #format to :custom" do
+      expect(message.format).to eq :default
+      message.usage = "usage"
+      expect(message.format).to eq :custom
+    end
+  end
 
   describe "#error" do
-    it "terminates the program with status 1" do
-      allow(STDERR).to receive(:puts) # Silence #error
-      expect { subject.error("Error message") }.to raise_error SystemExit
-    end
-
-    it "writes the message and usage on stderr" do
-      expected = "cmd: Error message\nUsage: cmd -a -b\n"
+    def expect_error(message)
       expect {
         begin
-          subject.error("Error message")
+          message.error "Error message"
         rescue SystemExit
         end
-      }.to output(expected).to_stderr
+      }
     end
 
-    context "if #usage has been assigned to" do
-      it "doesn't prefix the usage string with 'Usage: \#{program} '" do
-        subject.usage = "multiline\ndescription"
-        expected = "cmd: Error message\nmultiline\ndescription\n"
-        expect {
-          begin
-            subject.error("Error message")
-          rescue SystemExit
-          end
-        }.to output(expected).to_stderr
+    it "terminates the program with status 1" do
+      allow(STDERR).to receive(:print) # Silence #error
+      expect { message.error("Error message") }.to raise_error SystemExit
+    end
 
+    it "writes the message on stderr" do
+      expected = "cmd: Error message\nUsage: cmd -a -b\n"
+      expect_error(message).to output(expected).to_stderr
+    end
+
+    context "when usage is nil" do
+      it "doesn't print the usage" do
+        expected = "cmd: Error message\n"
+        message.usage = nil
+        message.format = :default
+        expect_error(message).to output(expected).to_stderr
+        message.format = :custom
+        expect_error(message).to output(expected).to_stderr
+      end
+    end
+
+    context "when format is :custom'" do
+      it "doesn't format the usage string" do
+        expected = "cmd: Error message\nmultiline\ndescription\n"
+        message = Messenger.new("cmd", "multiline\ndescription")
+        message.format = :custom
+        expect_error(message).to output(expected).to_stderr
       end
     end
   end
 
   describe "#fail" do
-    it "terminates the program with status 1" do
-      allow(STDERR).to receive(:puts) # Silence #error
-      expect { subject.fail("Error message") }.to raise_error SystemExit
-    end
-    it "writes the message on stderr" do
-      expected = "cmd: Error message\n"
+    def expect_fail(message)
       expect {
         begin
-          subject.fail("Error message")
+          message.fail "Error message"
         rescue SystemExit
         end
-      }.to output(expected).to_stderr
+      }
+    end
+
+    it "terminates the program with status 1" do
+      allow(STDERR).to receive(:puts) # Silence #error
+      expect { message.fail("Error message") }.to raise_error SystemExit
+    end
+
+    it "writes the message on stderr" do
+      expected = "cmd: Error message\n"
+      expect_fail(message).to output(expected).to_stderr
     end
   end
 end
