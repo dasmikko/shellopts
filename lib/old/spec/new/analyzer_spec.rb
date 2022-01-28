d2893453df5af9ffcfb4972dd03f2571a0d1771f
Claus Rasmussen <ras@danak.dk>
2022-01-28 16:11:12 +0100

    Also moved tests

---
+++ lib/old/spec/new/analyzer_spec.rb
@@ -0,0 +1,10 @@
+
+describe "ShellOpts" do
+  describe "Grammar" do
+    describe "Command" do
+      describe "#collect_options" do
+        it "fails if given both --with-separator and --with_separator"
+      end
+    end
+  end
+end
